2ed62bc86f999f95f0c66eb119a8248cc188edd0
mauricio.usatai@gmail.com <mauricio.usatai@gmail.com>
2023-09-15 16:09:16 -0300

    feat: better file names

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 import os
+from uuid import uuid4
 from dotenv import load_dotenv
 
 from pydantic_settings import BaseSettings
@@ -11,6 +12,7 @@ class Settings(BaseSettings):
     """App config"""
 
     DEPLOY: str = os.environ.get("DEPLOY", "local")
+    RUN_ID: str = os.environ.get("RUN_ID", str(uuid4()))
 
     # AWS config
     AWS_REGION: str = os.environ.get("AWS_REGION", "us-east-1")
